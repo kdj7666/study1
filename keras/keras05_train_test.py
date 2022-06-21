b3640afefe6c25e46d4db3ab10ec68bfcfdafbee
kdj7666 <107666085+kdj7666@users.noreply.github.com>
2022-06-21 13:30:34 +0900

    study 883

---
+++ keras/keras05_train_test.py
@@ -0,0 +1,27 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+#1. date
+# x = np.array([1.2.3.4.5.6.7.8.9.10])
+# y = np.array([1.2.3.4.5.6.7.8.9.10])
+x_train =np.array([1,2,3,4,5,6,7])
+x_test = np.array([8,9,10])
+y_train =np.array([1,2,3,4,5,6,7])
+y_test = np.array([8,9,10])
+
+#2. model             딥러닝 인풋1개 아웃풋1개 히든레이어 10개
+model = Sequential()
+model.add(Dense(10, input_dim=1))
+model.add(Dense(1))
+
+#3. 컴파일 , 훈련 
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train, y_train, epochs=300 , batch_size=1)
+
+#4. 평가 예측                                            # 훈련은 다 시켜주면 좋다 
+loss = model.evaluate(x_test, y_test)
+print('loss', loss)
+result = model.predict([11])
+print('11의 예측값 : ',result)
+
