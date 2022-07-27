6b04f1c83f0e607bcf19f714347dd682091d4ab6
kdj7666 <107666085+kdj7666@users.noreply.github.com>
2022-07-27 22:31:42 +0900

    2022-07-27-01

--- keras/keras53_project.py
+++ keras/keras53_project.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import time
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Conv2D
 from keras.preprocessing.image import ImageDataGenerator
@@ -36,4 +37,70 @@ xy_test = test_datagen.flow_from_directory(
     color_mode='grayscale',  # 컬러 작업 
     shuffle=True,
 )   # Found 3 images belonging to 1 classes.
+
+
+print(xy_train[0][0]) # (5, 150, 150, 3)
+print(xy_train[0][1])
+
+print(xy_train[0][0].shape, xy_train[0][1].shape)  # (80, 200, 200, 1) (80,)
+
+x_train = xy_train[0][0]
+y_train = xy_train[0][1]
+# x_test = xy_test[0][0]
+# y_test = xy_test[0][1]
+
+
+
+# print(type(xy_train))
+# print(type(xy_train[0]))
+# print(type(xy_train[0][0]))
+# print(type(xy_train[0][1]))
+
+augment_size = 480000        # 증강 사이즈 
+randidx = np.random.randint(x_train.shape[0], size=augment_size)  # np.random.randint = 무작위로 int(정수)값을 넣어준다 
+print(randidx) # [20762  8095 11489 ... 58612  1518 52314]
+
+
+
+print(y_train.shape) # 5, 
+
+
+#2. model
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten
+
+model = Sequential()
+model.add(Conv2D(64, (2,2), input_shape=(200, 200, 1), activation='relu'))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+#3. compile, epochs
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# model.fit(xy_train[0][0], xy_train[0][1]) # 허나 배치를 최대로 잡으면 이것도 가능하다
+
+start_time = time.time()
+
+hist = model.fit_generator(xy_train, epochs=10, steps_per_epoch=64,    
+                         # 스텝 펄 에포 ( 통상적으로 batch= 160/5 = 32)  # 훈련 배치 사이즈가 32가 넘어서도 돌아가긴한다 추가적 환경 제공 가능
+                    validation_data=xy_test,                    # 발리데이션 범주를 테스트로
+                    validation_steps=5)                         # 발리데이션 스텝 : 한 epoch 종료 시 마다 검증할 때 사용되는 검증 스텝 수를 지정합니다
+
+end_time = time.time()-start_time
+
+#4. evluate, predict
+acc = hist.history['accuracy']
+# val_accuracy = hist.history['val_accuracy']
+loss = hist.history['loss']
+# val_loss = hist.history['val_loss']
+
+print('loss : ', loss[-1]) # 마지막 괄호로 마지막 1개만 보겠다
+# print('val_loss : ', val_loss[-1])
+# print('val_accuracy : ', val_accuracy[-1])
+print('accuracy : ', acc[-1])
+print('걸린시간 : ', end_time)
 
